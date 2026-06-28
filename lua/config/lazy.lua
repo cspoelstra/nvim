96bfffc60df39bc0b5751b8570794991092103ec
Chris Spoelstra <christiaan.spoelstra@kiswe.com>
2026-06-28 15:11:38 -0700

    UPD

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,7 +27,8 @@ require("lazy").setup({
 		version = false, -- always use the latest git commit
 		-- version = "*", -- try installing the latest stable version for plugins that support semver
 	},
-	install = { colorscheme = { "catppuccin-latte" } },
+	-- install = { colorscheme = { "catppuccin-latte" } },
+	-- install = { colorscheme = { "catppuccin-mocha" } },
 	checker = { enabled = true, notify = false }, -- automatically check for plugin updates
 	performance = {
 		rtp = {
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,11 +4,44 @@ return {
 		"catppuccin/nvim",
 		-- tag = "v1.10.0", -- Pin to v1.10 to avoid a breaking change in V1.11 API
 		lazy = true,
+		opts = {
+			color_overrides = {
+				mocha = {
+					base = "#000000",
+					mantle = "#000000",
+					crust = "#000000",
+				},
+			},
+		},
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = true,
+	},
+	{
+		"metalelf0/jellybeans-nvim",
+		lazy = true,
+		dependencies = "rktjmp/lush.nvim",
+	},
+	{
+		"Mofiqul/dracula.nvim",
+		lazy = true,
+	},
+	{
+		"bluz71/vim-moonfly-colors",
+		name = "moonfly",
+		lazy = true,
 	},
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "catppuccin-latte",
+			--colorscheme = "catppuccin-latte",
+			--colorscheme = "catppuccin-mocha", -- true black
+			--colorscheme = "tokyonight-night",
+			--colorscheme = "jellybeans-nvim",
+			--colorscheme = "dracula",
+			colorscheme = "moonfly", -- true black
+			-- still need to check out: https://github.com/ray-x/starry.nvim (deep_black option)
 		},
 	},
 }
